769c579c350dabed2969fd7e866088ca72a21910
fzukorlic1 <fzukorlic1@etf.unsa.ba>
2021-05-07 14:53:42 +0200

    added registration

--- frontend/OnlineShop/src/app/app.module.ts
+++ frontend/OnlineShop/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './common/user.service';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
